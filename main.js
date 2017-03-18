afd4f35cc000951b771388cec76b10c91cc13861
Wyatt Calandro <arm1stice@arm1stice.com>
2017-03-18 14:40:55 -0700

    Added basic files
    Added README, LICENSE, added future config file to gitignore, and added the starting file that will launch bootloader
    
    Signed-off-by: Wyatt Calandro <arm1stice@arm1stice.com>

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+  LP-Tracker - A desktop application to help you track your LP
+  Created by Wyatt Calandro (Arm1stice)
+  Copyright (c) 2017 Wyatt Calandro
+*/
+
+// Hi, I'm Wyatt, and I think vanilla JS is ugly
+require('coffee-script').register()
+// Don't mind me, just invoking my CoffeeScript transpliler
+require('./src/boot/index') // Starting the main application
